d2ad52998b0c8160d72ad053830abadccf70243a
Oliver Hallam <oliver.hallam@gmail.com>
2013-12-02 22:58:39 +0000

    Initial checkin

---
+++ ReflectAsm/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+
+using Microsoft.Diagnostics.Runtime;
+
+namespace ReflectAsm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var proc = Process.Start("TestProcess.exe");
+            Thread.Sleep(1000);
+            //var proc = Process.GetProcessesByName("TestProcess")[0];
+            using (var target = DataTarget.AttachToProcess(proc.Id, 5000))
+            {
+                var dacLocation = target.ClrVersions[0].TryGetDacLocation();
+                var runtime = target.CreateRuntime(dacLocation);
+
+                var module = runtime.AppDomains.SelectMany(ad => ad.Modules).Single(m => m.Name.EndsWith("TestProcess.exe"));
+                var program = module.GetTypeByName("TestProcess.Program");
+                var method = program.Methods.Single(m => m.Name == "TestMethod");
+                var nativeCodeAddress = method.NativeCode;
+                var offsetMap = method.ILOffsetMap;
+                var finalAddress = offsetMap.Last().startAddress;
+                var size = (int)(finalAddress - nativeCodeAddress);
+
+                var bytes = new byte[size];
+                int bytesRead;
+                target.ReadProcessMemory(nativeCodeAddress, bytes, size, out bytesRead);
+                
+                for (var i = 0; i < bytesRead; i++)
+                {
+                    Console.WriteLine(ToHex(bytes[i]));
+                }
+            }
+
+            proc.Kill();
+
+            Console.ReadLine();
+        }
+
+        private static string ToHex(byte b)
+        {
+            const string hexstring = "0123456789ABCDEF";
+            return new string(new [] { hexstring[b / 16], hexstring[b % 16] });
+        }
+    }
+}
---
+++ TestProcess/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace TestProcess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var method = typeof(Program).GetMethod("TestMethod", BindingFlags.Static | BindingFlags.NonPublic);
+            RuntimeHelpers.PrepareMethod(method.MethodHandle);
+
+            Console.ReadLine();
+
+            TestMethod();
+        }
+
+        private static void TestMethod()
+        {
+            int sum = 0;
+            for (var i = 0; i < 10; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
